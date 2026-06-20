efcf1734cb1a9a4177de883e42d9527857a936fe
Thomas Jung <12159356+jung-thomas@users.noreply.github.com>
2026-06-20 18:16:54 -0400

    feat(admin): Tenant admin tile (#466)
    4-field tile with 3 LargeString textareas + 1 ComboBox for
    rebuildTargetEnv. ComboBox is the only enum-enforcement point
    (DB is unconstrained String(10)). Direct OData PATCH bypasses
    validation by design.

---
+++ app/admin/tenant/webapp/controller/Settings.controller.js
@@ -0,0 +1,85 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel",
+  "sap/m/MessageToast",
+  "sap/m/MessageBox"
+], function (Controller, JSONModel, MessageToast, MessageBox) {
+  "use strict";
+  return Controller.extend("sap.tutorials.admin.tenant.controller.Settings", {
+    onInit: function () {
+      var oJSON = new JSONModel({
+        allowedCorsOrigins: "",
+        rebuildTargetEnv: "dev",
+        techUsers: "",
+        techUsersMapping: ""
+      });
+      this.getView().setModel(oJSON, "settings");
+      this._loadSettings();
+    },
+
+    _loadSettings: function () {
+      var oModel = this.getView().getModel("settings");
+      fetch("/admin/TenantSettings", {
+        credentials: "include",
+        headers: { "Accept": "application/json" }
+      })
+        .then(function (res) {
+          if (!res.ok) { throw new Error("HTTP " + res.status); }
+          return res.json();
+        })
+        .then(function (data) {
+          oModel.setData({
+            allowedCorsOrigins: data.allowedCorsOrigins || "",
+            rebuildTargetEnv: data.rebuildTargetEnv || "dev",
+            techUsers: data.techUsers || "",
+            techUsersMapping: data.techUsersMapping || ""
+          });
+        })
+        .catch(function (err) {
+          MessageToast.show("Failed to load settings: " + err.message);
+        });
+    },
+
+    onReload: function () {
+      this._loadSettings();
+    },
+
+    onSave: function () {
+      var data = this.getView().getModel("settings").getData();
+      var body = {
+        allowedCorsOrigins: data.allowedCorsOrigins || null,
+        rebuildTargetEnv: data.rebuildTargetEnv || null,
+        techUsers: data.techUsers || null,
+        techUsersMapping: data.techUsersMapping || null
+      };
+
+      fetch("/admin/$metadata", {
+        method: "HEAD",
+        credentials: "include",
+        headers: { "x-csrf-token": "fetch" }
+      })
+        .then(function (res) {
+          return res.headers.get("x-csrf-token") || "";
+        })
+        .then(function (token) {
+          return fetch("/admin/TenantSettings", {
+            method: "PATCH",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json",
+              "x-csrf-token": token
+            },
+            body: JSON.stringify(body)
+          });
+        })
+        .then(function (res) {
+          if (!res.ok) { throw new Error("HTTP " + res.status); }
+          MessageToast.show("Saved");
+        })
+        .catch(function (err) {
+          MessageBox.error("Save failed: " + err.message);
+        });
+    }
+  });
+});
